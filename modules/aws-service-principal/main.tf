492ad9f5acd800135100f82dcfc3c47ff4c9f0c5
Arpit Jasapara <arpit.jasapara@databricks.com>
2022-07-12 15:49:41 -0400

    Update provider constraint

--- modules/aws-service-principal/main.tf
+++ modules/aws-service-principal/main.tf
@@ -9,7 +9,7 @@ variable "group_name" {
 }
 
 resource "databricks_service_principal" "sp" {
-  display_name = var.display_name
+  display_name         = var.display_name
   allow_cluster_create = true
 }
 
